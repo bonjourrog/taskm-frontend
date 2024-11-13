62fc9aca4f67ab99e372c26fade85f30a58cce2e
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-12 18:05:44 -0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <p className="text-blue-400 font-bold text-2xl">App Works</p>
+  )
+}
+
+export default App
