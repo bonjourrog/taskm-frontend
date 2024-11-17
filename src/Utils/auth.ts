3c671b1d762869b9ba09f75895fb9a179dbb7f12
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-17 16:13:33 -0700

    feat: create setAuthenticatedUSer function to set user information when user sign in

--- src/Utils/auth.ts
+++ src/Utils/auth.ts
@@ -1,7 +1,17 @@
+import { User } from "../Entity/user";
+
 export const isAuthenticated = ():boolean=>{
     const token:string|null = localStorage.getItem("authToken");
     if(!token)return false;
-    const payload = JSON.parse(atob(token.split('.')[1]))
-    const isExpired: boolean = payload.exp * 1000 < Date.now()
+    const payload: User = JSON.parse(atob(token.split('.')[1]));
+    const isExpired: boolean = payload.exp * 1000 < Date.now();
     return !isExpired;
+}
+export const setAuthenticatedUser = ():User=>{
+    const token: string | null = localStorage.getItem("authToken");
+    if (token) {
+        const payload: User = JSON.parse(atob(token.split('.')[1]));
+        return payload
+    }
+    return {} as User;
 }
