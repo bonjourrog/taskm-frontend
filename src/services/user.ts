b93eb3908271acb985dc6fe5f878e131cc2f6abb
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-17 16:15:13 -0700

    feat: add getAll function into user service to get all list created by user id

--- src/services/user.ts
+++ src/services/user.ts
@@ -7,7 +7,13 @@ const createList = async(list:Partial<List>, user_id:string):Promise<Response>=>
     const {data} = await api.post('/list',{name:list.name, color:list.color, user_id:user_id},{headers:{Authorization:`${token}`}});
     return data;
 }
+const getAll = async(user_id:string):Promise<List[]>=>{
+    const token: string = localStorage.getItem("authToken") as string;
+    const response = await api.get(`/list/${user_id}`, {headers:{Authorization:token}});
+    return response.data
+}
 
 export const  userService = {
-    createList
+    createList,
+    getAll
 }
