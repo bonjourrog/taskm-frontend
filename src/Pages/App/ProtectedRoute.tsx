f8d31c882fc4822993a2819df27d5d0896b228fd
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-17 16:25:40 -0700

    feat: set user info if user is autenticated in protectedRoute component

--- src/Pages/App/ProtectedRoute.tsx
+++ src/Pages/App/ProtectedRoute.tsx
@@ -1,7 +1,16 @@
 import { Navigate, Outlet } from "react-router-dom";
-import { isAuthenticated } from "../../Utils/auth";
+import { isAuthenticated, setAuthenticatedUser } from "../../Utils/auth";
+import { useEffect } from "react";
+import useUserStore from "../../store/useUserStore";
 
 const ProtectedRoute:React.FC = ()=>{
+    const {setUser} = useUserStore()
+    useEffect(()=>{
+        if(isAuthenticated()){
+            const user = setAuthenticatedUser()
+            setUser(user);
+        }
+    },[])
     return isAuthenticated()?<Outlet/>:<Navigate to={'/sign-in'}/>
 }
 export default ProtectedRoute;
