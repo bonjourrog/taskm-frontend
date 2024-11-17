caf8ad8ba38fc5b4ac1c89663ec70c63692fbd93
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-17 16:22:33 -0700

    refactor: remove unused properties in signin component

--- src/Pages/Signin/index.tsx
+++ src/Pages/Signin/index.tsx
@@ -1,6 +1,6 @@
 import './Signin.css';
 import { SigninProps } from './Signin.props';
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import AuthForm from '../../Components/AuthForm';
 import { ChangeEvent, FormEvent, useState } from 'react';
 import { signin } from '../../services/auth';
