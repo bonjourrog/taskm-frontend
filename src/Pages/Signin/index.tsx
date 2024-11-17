8a749bb214d9593290cb1bf6d93121b89471c3e6
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-16 21:40:06 -0700

    feat: implement input onChange handler and implement signin from service

--- src/Pages/Signin/index.tsx
+++ src/Pages/Signin/index.tsx
@@ -1,19 +1,40 @@
 import './Signin.css';
 import { SigninProps } from './Signin.props';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 import AuthForm from '../../Components/AuthForm';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { signin } from '../../services/auth';
 
 const Signin: React.FC<SigninProps> = ()=>{
+    const navigate = useNavigate();
+    const [formData, setFormData] = useState<{email:string, password:string}>({email:"",password:""})
+    const handleOnChangeInput = (event:ChangeEvent<HTMLInputElement>)=>{
+        setFormData({...formData, [event.target.name]:event.target.value})
+    }
+    const handleOnSubmit = async(event:FormEvent<HTMLFormElement>)=>{
+        event.preventDefault();
+        try {
+            const response = await signin(formData.email, formData.password)
+            if(response && !response.error){
+                localStorage.setItem("authToken", response.data)
+                navigate('/');
+            }
+        } catch (error:any) {
+            console.log(error.response.data);
+            
+        }
+        
+    }
     return <main className="signin">
         <AuthForm headline={{first:"Ready to get", last:"things done?"}} caption='Sign in to manage your tasks and keep your productivity flowing.'>
-            <form>
+            <form onSubmit={handleOnSubmit}>
                 <label htmlFor="" className='w-full text-zinc-700 font-bold text-sm'>
                     email
-                    <input type="email" name="email" className='input'/>
+                    <input onChange={handleOnChangeInput} type="email" name="email" className='input'/>
                 </label>
                 <label htmlFor="" className='w-full text-zinc-700 font-bold text-sm'>
                     Password
-                    <input type="password" name="password" className='input'/>
+                    <input onChange={handleOnChangeInput} type="password" name="password" className='input'/>
                 </label>
                 <input type="submit" value="Login" className='input input--btn'/>
             </form>
