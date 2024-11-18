88d75856422e9bd084985badbee4690ef577093e
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-17 22:17:05 -0700

    refactor: add trycatch into signin function in auth server and remove null returning

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,9 +1,13 @@
 import { Response } from '../Entity/response';
 import api from './api';
 
-export const signin = async(email:string, password:string):Promise<Response|null>=>{
-    const {data} = await api.post('auth/sign-in',{email, password});
-    return data;
+export const signin = async(email:string, password:string):Promise<Response>=>{
+    try {
+        const {data} = await api.post('auth/sign-in',{email, password});
+        return data;
+    } catch (error:any) {
+        return error.response.data
+    }
 }
 export const signup = async(user:{email:string, password:string,user_name:string}):Promise<Response>=>{
     try {
