23062bde2236aa688cf804e1756acdfa08ef0b59
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-18 10:05:19 -0700

    chore: remove unused properties in dialog component

--- src/Components/Dialog/index.tsx
+++ src/Components/Dialog/index.tsx
@@ -1,7 +1,7 @@
 import './Dialog.css';
 import { DialogProps } from './Dialog.props';
 
-const Dialog: React.FC<DialogProps> = ({headline, caption, bgColor, txtColor, className})=>{
+const Dialog: React.FC<DialogProps> = ({headline, className})=>{
     return <p className={`fixed top-10 left-1/2 transform -translate-x-1/2 py-2 px-6 rounded-lg bg-app-green text-white front-bold z-20 ${className}`}>{headline}</p>
 }
 export default Dialog;
