2ea3010da3102f54d601436c38b115425c309141
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-17 21:40:16 -0700

    feat: create isEmailValid function, for email validation, into auth util

--- src/Utils/auth.ts
+++ src/Utils/auth.ts
@@ -15,3 +15,7 @@ export const setAuthenticatedUser = ():User=>{
     }
     return {} as User;
 }
+export const isEmailValid = (email:string):boolean=>{
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+    return emailRegex.test(email)
+}
