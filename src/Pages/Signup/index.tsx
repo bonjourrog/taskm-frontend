7c7b1aa64a4827472ae13aebb580d2dc4500a172
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-17 21:48:17 -0700

    feat: implement signup functionality in signup component

--- src/Pages/Signup/index.tsx
+++ src/Pages/Signup/index.tsx
@@ -1,74 +1,72 @@
-import { HiClipboardList } from 'react-icons/hi';
+import { ChangeEvent, FormEvent, useState } from 'react';
 import AuthForm from '../../Components/AuthForm';
 import './Signup.css';
 import { SignupProps } from './Signup.props';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { signup } from '../../services/auth';
+import { isEmailValid } from '../../Utils/auth';
+
+interface FormData{
+    user_name:string;
+    email:string;
+    password:string;
+    verify_password:string;
+}
 
 const Signup: React.FC<SignupProps> = ()=>{
+    const navigate = useNavigate();
+    const [message, setMessage] = useState<{message:string, show:boolean}>({message:"", show:false});
+    const [formData, setFormData] = useState<FormData>({email:"",password:"",user_name:"", verify_password:""});
+    const handleOnChange = (event:ChangeEvent<HTMLInputElement>)=>{
+        const {name, value} = event.target;
+        setFormData({...formData, [name]:value});
+    }
+    const handleOnSubmit = async(event:FormEvent<HTMLFormElement>)=>{
+        event.preventDefault();
+        try {
+            const {email,password, user_name, verify_password} = formData;
+            if( user_name.length<5)throw new Error('user name must be al least 5 characters');
+            if(password.length<5)throw new Error('password must be al least 5 characters');
+            if(password !== verify_password)throw new Error('please verify password');
+            if(!password || !email || !user_name)throw new Error('please fill all fields');
+            if(!isEmailValid(email))throw new Error('invalid email');
+            const res = await signup({...formData});
+            console.log(res);
+            
+            if(res.error)throw new Error(res.message);
+            navigate('/sign-in')
+        } catch (error) {
+            const errorMessage = error instanceof Error ? error.message:'Unkown error';
+            setMessage({message:errorMessage, show:true})
+            setTimeout(()=>{
+                setMessage({message:'', show:false})
+            },3000)
+        }
+    }
     return <main className="signup">
+    {message.show?<p className='fixed top-10 left-1/2 transform -translate-x-1/2 py-2 px-6 rounded-lg bg-app-green text-white front-bold z-20'>{message.message}</p>:undefined}
     <AuthForm headline={{first:"Join us and", last:"get organized"}} caption='Create an account to start managing your tasks efficiently.'>
-            <form>
+            <form onSubmit={handleOnSubmit}>
                 <label htmlFor="" className='w-full text-zinc-700 font-bold text-sm'>
                     Email
-                    <input type="email" name="email" className='input' />
+                    <input onChange={handleOnChange} value={formData.email} type="email" name="email" className='input' />
                 </label>
                 <label htmlFor="" className='w-full text-zinc-700 font-bold text-sm'>
                     User
-                    <input type="text" name="email" className='input' />
+                    <input onChange={handleOnChange} value={formData.user_name} type="text" name="user_name" className='input' />
                 </label>
                 <label htmlFor="" className='w-full text-zinc-700 font-bold text-sm'>
                     Password
-                    <input type="password" name="password" className='input' />
+                    <input onChange={handleOnChange} value={formData.password} type="password" name="password" className='input' />
                 </label>
                 <label htmlFor="" className='w-full text-zinc-700 font-bold text-sm'>
                     Verify password
-                    <input type="password" name="password" className='input' />
+                    <input onChange={handleOnChange} value={formData.verify_password} type="password" name="verify_password" className='input' />
                 </label>
                 <input type="submit" value="Register" className='input input--btn' />
             </form>
             <Link className='signup-btn' to={"/sign-in"}>Sign in</Link>
     </AuthForm>
 </main>
-    // return <main className="signup">
-    //     <div className='signup__container'>
-    //     <header className="header">
-    //             <HiClipboardList className="header__icon"/>
-    //             <span className="header__text">TASKM</span>
-    //         </header>
-    //         <AuthForm headline={{first:"Join us and", last:"get organized"}} caption='Create an account to start managing your tasks efficiently.'>
-    //             <>
-    //                 <form>
-    //                     <label htmlFor="" className='w-full text-zinc-700 font-bold text-sm'>
-    //                         Email
-    //                         <input type="email" name="email" className='input'/>
-    //                     </label>
-    //                     <label htmlFor="" className='w-full text-zinc-700 font-bold text-sm'>
-    //                         User
-    //                         <input type="text" name="email" className='input'/>
-    //                     </label>
-    //                     <label htmlFor="" className='w-full text-zinc-700 font-bold text-sm'>
-    //                         Password
-    //                         <input type="password" name="password" className='input'/>
-    //                     </label>
-    //                     <label htmlFor="" className='w-full text-zinc-700 font-bold text-sm'>
-    //                         Verify password
-    //                         <input type="password" name="password" className='input'/>
-    //                     </label>
-    //                     <input type="submit" value="Login" className='input input--btn'/>
-    //                 </form>
-    //                 <Link className='signup-btn' to={"/signup"}>create an account</Link>
-    //             </>
-    //         </AuthForm>
-    //     </div>
-    //     {
-    //             innerWidth > 1000? <section className='signup__leftside'>
-    //                 <div className='relative w-full h-full rounded-lg overflow-hidden'>
-    //                     <p className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-bold text-5xl text-center'>Your projects <br />
-    //                     are waiting</p>
-    //                     <img src="https://res.cloudinary.com/dcezb5utw/image/upload/v1731604271/taskm/cbdnrxkmgbjtrotp6v1c.png" alt="todo list image" className='object-cover w-full h-full'/>
-    //                 </div>
-    //         </section>:undefined
-    //         }
-    // </main>
 }
 export default Signup;
