45cce570c8bbad5da4d09d851a0259bb6cbdc7cc
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-18 17:12:24 -0700

    feat: create removeToken function in util

--- src/Utils/auth.ts
+++ src/Utils/auth.ts
@@ -19,3 +19,6 @@ export const isEmailValid = (email:string):boolean=>{
     const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
     return emailRegex.test(email)
 }
+export const removeToken = ()=>{
+    localStorage.removeItem("authToken")
+}
