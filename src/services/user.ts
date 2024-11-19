ce94295af130d6425fe96a8c0dcb0081e9377c61
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-18 18:13:58 -0700

    refactorL add a try catch in createList function in user service

--- src/services/user.ts
+++ src/services/user.ts
@@ -3,9 +3,13 @@ import { List } from '../Entity/list';
 import api from './api';
 
 const createList = async(list:Partial<List>, user_id:string):Promise<Response>=>{
-    const token: string = localStorage.getItem("authToken") as string;
-    const {data} = await api.post('/list',{name:list.name, color:list.color, user_id:user_id},{headers:{Authorization:`${token}`}});
-    return data;
+    try {
+        const token: string = localStorage.getItem("authToken") as string;
+        const {data} = await api.post('/list',{name:list.name, color:list.color, user_id:user_id},{headers:{Authorization:`${token}`}});
+        return data;
+    } catch (error:any) {
+        return error.response.data
+    }
 }
 const getAll = async(user_id:string):Promise<List[]>=>{
     const token: string = localStorage.getItem("authToken") as string;
