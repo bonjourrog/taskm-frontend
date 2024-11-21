22436b3b3ed6b5befab4712422fd6e9aa13cc321
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-21 16:41:42 -0700

    feat: validate if token is equal to undefined return false in isAuthenticated function

--- src/Utils/auth.ts
+++ src/Utils/auth.ts
@@ -2,7 +2,7 @@ import { User } from "../Entity/user";
 
 export const isAuthenticated = ():boolean=>{
     const token:string|null = localStorage.getItem("authToken");
-    if(!token)return false;
+    if(!token || token === 'undefined')return false;
     const payload: User = JSON.parse(atob(token.split('.')[1]));
     const isExpired: boolean = payload.exp * 1000 < Date.now();
     return !isExpired;
