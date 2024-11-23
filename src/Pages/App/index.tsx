6b8ae61cbcee81d72a8702ba63edeb455b897b80
Rogelio Beltrán <rbv.rogelio.beltran@gmail.com>
2024-11-23 16:41:17 -0700

    feat: initialize aos animation in App component

--- src/Pages/App/index.tsx
+++ src/Pages/App/index.tsx
@@ -1,8 +1,11 @@
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import { AppRoutes } from './App.routes'
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 function App() {
+  AOS.init();
   return (
     <BrowserRouter>
       <AppRoutes/>
