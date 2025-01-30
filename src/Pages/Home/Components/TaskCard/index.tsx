a919d79dd1d9536e4a6d16f7f63d6ac08b3e5717
Rogelio Beltran <rogelio.beltran@bonjourog.com>
2025-01-29 19:48:42 -0800

    feat: implement feature to update task description

--- src/Pages/Home/Components/TaskCard/index.tsx
+++ src/Pages/Home/Components/TaskCard/index.tsx
@@ -13,7 +13,9 @@ const TaskCard: React.FC<TaskCardProps> = ({task})=>{
     const {setTasks, tasks} = useTaskStore();
     const {activeItem} = useListStore();
     const [editTask, setEditTask] = useState<boolean>(false);
+    const [editTaskDesc, setEditTaskDesc] = useState<boolean>(false);
     const [showDescription, setShowDescription] = useState<boolean>(false);
+    const [taskDesc, setTaskDesc] = useState<string>(task.description)
     const [taskName, setTaskName] = useState<string>("");
     const handleTaskUpdate = async(_task:Task)=>{
         try {
@@ -46,23 +48,28 @@ const TaskCard: React.FC<TaskCardProps> = ({task})=>{
         })
         setTasks(_tasks)
     }
-    const handleOnBlur = (event:FocusEvent<HTMLInputElement>)=>{
-        event.preventDefault()
+    const handleOnBlur = (_task:Task)=>{
         setEditTask(false);
-        handleTaskUpdate({...task, name:taskName});
+        setEditTaskDesc(false)
+        handleTaskUpdate(_task);
     }
     useEffect(()=>{
         setTaskName(task.name)
     }, [])
     return <div data-aos="zoom-out" className='task-card'>
-        {showDescription?<div className='task-description'>{task.description}</div>:undefined}
+        {
+            showDescription?
+                editTaskDesc?<textarea onBlur={()=>handleOnBlur({...task, description:taskDesc})} className='task-description outline-emerald-100' autoFocus value={taskDesc} onChange={({target})=>setTaskDesc(target.value)}/>
+                :<div onDoubleClick={()=>setEditTaskDesc(true)} className='task-description'>{taskDesc}</div>
+            :undefined
+        }
         <div className='task-card__leftside'>
             <div onClick={()=>{toggleTask({...task, done:!task.done})}} className={`task-card__checkbox ${task.done?'bg-app-green':'bg-white'}`}>
                 {task.done?<FaCheck className='task-card__checkbox-icon'/>:undefined}
             </div>
             <div className='task-card__title'>
                 <span className='task-card__tooltip'>Double click to edit</span>
-                {editTask?<input className='task-card__input' autoFocus onBlur={handleOnBlur} type="text" value={taskName} onChange={({target})=>setTaskName(target.value)} />:<p onDoubleClick={()=>setEditTask(true)}>{taskName}</p>}
+                {editTask?<input className='task-card__input' autoFocus onBlur={()=>handleOnBlur({...task, name:taskName})} type="text" value={taskName} onChange={({target})=>setTaskName(target.value)} />:<p onDoubleClick={()=>setEditTask(true)}>{taskName}</p>}
             </div>
         </div>
         <div className='task-card__rightside'>
